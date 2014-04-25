1fc01bbac1fe3bc0a39ef2cd1c2bdb981de938a3
hraboviyvadim <hraboviyvadim@gmail.com>
2014-04-25 18:07:41 +0300

    tabs js

--- js/common.js
+++ js/common.js
@@ -32,9 +32,49 @@ $(document).ready(function() {
             });
         });
     }
+
+    function tab2() {
+       $(".js-tab2").each(function(){
+         var tab_link = $(this).find("a");
+         var tab_cont = $(this).parent().parent(".js-tab-group").find(".js-tab-cont");
+         tab_cont.hide();
+         $(this).parents(".js-tab-group").find(".js-tab1").show();
+            tab_link.on("click", function() {
+                var index = $(this).attr("href");
+                tab_link.removeClass("is-active");
+                tab_link.parent().removeClass("is-active");
+                $(this).addClass("is-active");
+                $(this).parent().addClass("is-active");
+                tab_cont.hide();
+                $(this).parent().parent().parent().parent(".js-tab-group").find("."+index).show();
+                return false;
+            });
+        });
+    }
     
+    function tab3() {
+       $(".js-tab3").each(function(){
+         var tab_link = $(this).find("a");
+         var tab_cont = $(this).parent(".js-tab-group").find(".js-tab-cont");
+         tab_cont.hide();
+         $(this).parent(".js-tab-group").find(".js-tab1").show();
+            tab_link.on("click", function() {
+                var index = $(this).attr("href");
+                tab_link.removeClass("is-active");
+                tab_link.parent().removeClass("is-active");
+                $(this).addClass("is-active");
+                $(this).parent().addClass("is-active");
+                tab_cont.hide();
+                $(this).parent().parent().parent(".js-tab-group").find("."+index).show();
+                return false;
+            });
+        });
+    }
+
     if ($(".js-tab-group").length) {
         tab();
+        tab2();
+        tab3();
     };
 
 
