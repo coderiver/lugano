409c0eae0d4502a7be3daf42d33fcbdf5f8019c7
Valera19900 <kaktuz34@gmail.com>
2014-04-25 18:26:34 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -3,15 +3,34 @@ $(document).ready(function() {
 	// btn top
 
 	$('.js-btntop').click(function(){
-	 	$('.js-btntop').hide();
-	 	$('.js-logo').show();
-	 	$('.js-btnbot').show();
-	 })
-	 $('.js-btnbot').click(function(){
-	 	$('.js-logo').hide();
-	 	$('.js-btnbot').hide();
-	 	$('.js-btntop').show();
-	 })
+        $('.js-btntop').slideUp();
+        $('.js-logo').slideDown();
+        $('.js-btnbot').slideDown();
+     })
+     $('.js-btnbot').click(function(){
+        $('.js-logo').slideUp();
+        $('.js-btnbot').slideUp();
+        $('.js-btntop').slideDown();
+     })
+    $('.js-dev').click(function(){
+        $('.js-footer').slideToggle();
+        $('.js-bg').toggleClass('is-active');
+        return false;
+     })
+    var form_trigger = $('.js-form-trigger');
+    form_trigger.on('click', function(){
+        $(this).toggleClass('is-active');
+        $(this).next().slideToggle();
+    });
+
+    form_trigger.parent().click(function (e) {
+      e.preventDefault();
+      return false;
+    });
+
+    $(document).on('click', function(){
+        form_trigger.next().slideUp();
+    })
 
 	// tabs
     function tab() {
