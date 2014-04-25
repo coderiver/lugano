a5b5131b53c598f7b0ebcf897b8e6ea474e37345
hraboviyvadim <hraboviyvadim@gmail.com>
2014-04-25 16:55:59 +0300

    content

--- js/common.js
+++ js/common.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-	console.log($('body').html());
+	
+	// btn top
+
 	$('.js-btntop').click(function(){
 	 	$('.js-btntop').hide();
 	 	$('.js-logo').show();
@@ -10,4 +12,30 @@ $(document).ready(function() {
 	 	$('.js-btnbot').hide();
 	 	$('.js-btntop').show();
 	 })
+
+	// tabs
+    function tab() {
+       $(".js-tab").each(function(){
+         var tab_link = $(this).find("a");
+         var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+         tab_cont.hide();
+         $(this).parents(".js-tab-group").find(".js-tab1").show();
+            tab_link.on("click", function() {
+                var index = $(this).attr("href");
+                tab_link.removeClass("is-active");
+                tab_link.parent().removeClass("is-active");
+                $(this).addClass("is-active");
+                $(this).parent().addClass("is-active");
+                tab_cont.hide();
+                $(this).parents(".js-tab-group").find("."+index).show();
+                return false;
+            });
+        });
+    }
+    
+    if ($(".js-tab-group").length) {
+        tab();
+    };
+
+
 });
