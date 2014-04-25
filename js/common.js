094a71258f26fccfc3578ae5d18d84869fe7e62b
Valera19900 <kaktuz34@gmail.com>
2014-04-25 11:23:38 +0300

    main-page

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 	console.log($('body').html());
+	$('.js-btntop').click(function(){
+	 	$('.js-btntop').hide();
+	 	$('.js-logo').show();
+	 	$('.js-btnbot').show();
+	 })
+	 $('.js-btnbot').click(function(){
+	 	$('.js-logo').hide();
+	 	$('.js-btnbot').hide();
+	 	$('.js-btntop').show();
+	 })
 });
