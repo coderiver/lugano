882040bf374f42a0b5f65e1c291a660bfeefc711
Valera19900 <kaktuz34@gmail.com>
2014-04-25 18:14:05 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,13 +1,33 @@
 $(document).ready(function() {
 	console.log($('body').html());
 	$('.js-btntop').click(function(){
-	 	$('.js-btntop').hide();
-	 	$('.js-logo').show();
-	 	$('.js-btnbot').show();
+	 	$('.js-btntop').slideUp();
+	 	$('.js-logo').slideDown();
+	 	$('.js-btnbot').slideDown();
 	 })
 	 $('.js-btnbot').click(function(){
-	 	$('.js-logo').hide();
-	 	$('.js-btnbot').hide();
-	 	$('.js-btntop').show();
+	 	$('.js-logo').slideUp();
+	 	$('.js-btnbot').slideUp();
+	 	$('.js-btntop').slideDown();
 	 })
+	$('.js-dev').click(function(){
+	 	$('.js-footer').slideToggle();
+	 	$('.js-bg').toggleClass('is-active');
+	 	return false;
+	 })
+	var form_trigger = $('.js-form-trigger');
+	form_trigger.on('click', function(){
+		$(this).toggleClass('is-active');
+		$(this).next().slideToggle();
+	});
+
+	form_trigger.parent().click(function (e) {
+	  e.preventDefault();
+	  return false;
+	});
+
+	$(document).on('click', function(){
+		form_trigger.next().slideUp();
+	})
+
 });
